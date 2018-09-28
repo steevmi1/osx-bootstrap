0d5545171625e1f879bc8ec7b90043eb64f6ab8a
steevmi1 <michael@steeves.io>
2018-09-28 11:23:59 -0400

    Script to install Yihui Xie's TinyTex, and then to install needed packages.

---
+++ tex.sh
@@ -0,0 +1,13 @@
+##  Install of Yihui Xie's TinyTex
+curl -sL "https://yihui.name/gh/tinytex/tools/install-unx.sh" | sh
+/usr/local/bin/tlmgr install memoir
+/usr/local/bin/tlmgr install ifetex
+/usr/local/bin/tlmgr install babel-english
+/usr/local/bin/tlmgr install csquotes
+/usr/local/bin/tlmgr install xcolor
+/usr/local/bin/tlmgr install soul
+/usr/local/bin/tlmgr install biblatex-chicago
+/usr/local/bin/tlmgr install biblatex
+/usr/local/bin/tlmgr install logreq
+/usr/local/bin/tlmgr install xstring
+/usr/local/bin/tlmgr install fontawesome
