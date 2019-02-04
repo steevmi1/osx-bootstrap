419a32d5938f2bc33531e034101b2bb1d7a34bba
Michael Steeves <michael@steeves.io>
2019-02-04 11:35:16 -0500

    Install spaceship theme and make available for use.

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+##  Custom theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
