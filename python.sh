3a8298442999aa3ad2d682f9cf5debe26696dbcc
Michael Steeves <michael@steeves.io>
2019-02-08 10:41:22 -0500

    Update installing python venvs. Still need to figure out what to do with TensorFlow.

--- python.sh
+++ python.sh
@@ -1,45 +1,43 @@
 #!/bin/sh
 
-if ! test -d $HOME/.local/venvs; then
-    mkdir -p $HOME/.local/venvs
+if ! test -d $HOME/venvs; then
+    mkdir -p $HOME/venvs
 else
-    if test -d $HOME/.local/venvs/cloud; then
-        mv $HOME/.local/venvs/cloud $HOME/.local/venvs/cloud.bak.$(date '+%Y%m%d.%H%M%S')
+    if test -d $HOME/venvs/cloud; then
+        mv $HOME/venvs/cloud $HOME/venvs/cloud.bak.$(date '+%Y%m%d.%H%M%S')
     fi
-    if test -d $HOME/.local/venvs/pelican; then
-        mv $HOME/.local/venvs/pelican $HOME/.local/venvs/pelican.bak.$(date '+%Y%m%d.%H%M%S')
+    if test -d $HOME/venvs/pelican; then
+        mv $HOME/venvs/pelican $HOME/venvs/pelican.bak.$(date '+%Y%m%d.%H%M%S')
     fi
-    if test -d $HOME/.local/venvs/ML; then
-        mv $HOME/.local/venvs/ML $HOME/.local/venvs/ML.bak.$(date '+%Y%m%d.%H%M%S')
+    if test -d $HOME/venvs/ML; then
+        mv $HOME/venvs/ML $HOME/venvs/ML.bak.$(date '+%Y%m%d.%H%M%S')
     fi
 fi
 
-brew install python3
-brew install graphviz --with-bindings
-
+brew install graphviz
+pip3 install --upgrade pip
 pip3 install pygments
 
-python3.6 -m venv $HOME/.local/venvs/cloud && source $HOME/.local/venvs/cloud/bin/activate
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade wheel
-pip install awscli
-pip install cfncluster
-pip install ansible
+python3 -m venv $HOME/venvs/cloud && source $HOME/venvs/cloud/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade wheel
+pip3 install awscli
+pip3 install aws-parallelcluster
+pip3 install ansible
 deactivate
 
-python3.6 -m venv $HOME/.local/venvs/pelican && source $HOME/.local/venvs/pelican/bin/activate
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade wheel
-pip install pelican==3.6.3
-pip install markdown rpy2
+python3 -m venv $HOME/venvs/pelican && source $HOME/venvs/pelican/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade wheel
+pip3 install pelican==3.6.3
+pip3 install markdown rpy2
 deactivate
 
-python3.6 -m venv $HOME/.local/venvs/ML && source $HOME/.local/venvs/ML/bin/activate
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install --upgrade wheel
-pip install jupyter tensorflow scikit-learn mxnet graphviz keras pandas matplotlib seaborn h5py scikit-image
-pip install orange3
+python3 -m venv $HOME/venvs/datasci && source $HOME/venvs/datasci/bin/activate
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install --upgrade wheel
+pip3 install jupyter scikit-learn mxnet keras pandas matplotlib seaborn h5py scikit-image torch torchvision pymc3 theano
 deactivate
